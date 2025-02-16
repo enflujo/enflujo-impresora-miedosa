e6fa5890d7e1f5ade3355870be1c99db94f01d6b
Juan Camilo González <info@juancgonzalez.com>
2025-02-16 12:27:51 +0100

    👷 Base para buscador de imágenes genéricas

--- src/componentes/BuscarImagenes.ts
+++ src/componentes/BuscarImagenes.ts
@@ -1,1 +1,83 @@
-export function buscarImagen() {}
+import { createClient } from 'pexels';
+import { pexelsLlave, unsplashLlaveAcceso } from '../secretos';
+import { type ColorId, createApi } from 'unsplash-js';
+
+const contenedor = document.getElementById('imagenesGenericas') as HTMLDivElement;
+const coloresImgs = document.getElementById('coloresImgs') as HTMLSelectElement;
+let color: ColorId | null = null;
+let termino: string | null = null;
+let numeroImgsPexels = 5;
+let numeroImgsUnsplash = 5;
+
+coloresImgs.addEventListener('change', () => {
+  color = coloresImgs.value.length ? (coloresImgs.value as ColorId) : null;
+  buscar();
+});
+
+export function buscarImagen(nombre: string, numPexels = 5, numUnsplash = 5) {
+  termino = nombre;
+  numeroImgsPexels = numPexels;
+  numeroImgsUnsplash = numUnsplash;
+  buscar();
+}
+
+function buscar() {
+  if (!termino) return;
+  contenedor.innerHTML = '';
+  buscarImagenesPexels(termino, numeroImgsPexels);
+  buscarImagenesUnsplash(termino, numeroImgsUnsplash);
+}
+
+async function buscarImagenesPexels(categoria: string, cantidad: number) {
+  const pexels = createClient(pexelsLlave);
+  const nombre = document.createElement('h3');
+  const contenedorImgs = document.createElement('div');
+
+  contenedorImgs.classList.add('imagenes');
+  nombre.textContent = 'Pexels';
+  contenedorImgs.appendChild(nombre);
+  contenedor.appendChild(contenedorImgs);
+
+  const res = await pexels.photos.search({ query: categoria, per_page: cantidad, color: color ? color : undefined });
+  if ('photos' in res && res.photos.length) {
+    res.photos.forEach((foto) => {
+      const img = new Image();
+      img.onload = () => {
+        img.style.height = '200px';
+        contenedorImgs.appendChild(img);
+      };
+      img.src = foto.src.original;
+    });
+  } else {
+    console.log('No se encontraron imágenes');
+  }
+}
+
+async function buscarImagenesUnsplash(categoria: string, cantidad: number) {
+  try {
+    const api = createApi({ accessKey: unsplashLlaveAcceso });
+    const res = await api.search.getPhotos({ query: categoria, perPage: cantidad, color: color ? color : undefined });
+    const nombre = document.createElement('h3');
+    const contenedorImgs = document.createElement('div');
+
+    contenedorImgs.classList.add('imagenes');
+    nombre.textContent = 'Unsplash';
+    contenedorImgs.appendChild(nombre);
+    contenedor.appendChild(contenedorImgs);
+
+    if ('response' in res && res.response?.results.length) {
+      res.response.results.forEach((foto) => {
+        const img = new Image();
+        img.onload = () => {
+          img.style.height = '200px';
+          contenedorImgs.appendChild(img);
+        };
+        img.src = foto.urls.raw;
+      });
+    } else {
+      console.log('No se encontraron imágenes');
+    }
+  } catch (error) {
+    console.log('Error al cargar imágenes de Unsplash:', error);
+  }
+}
--- src/programa.ts
+++ src/programa.ts
@@ -1,15 +1,17 @@
 // import { cargarImagenes, categorias } from './categorias';
+import { buscarImagen } from './componentes/BuscarImagenes';
 import { crearMenuCategorias } from './componentes/MenuCategorias';
 import VisorTrayectos from './componentes/VisorTrayectos';
 import './scss/estilos.scss';
 
 const previsualizador = new VisorTrayectos(document.getElementById('previsualizador') as HTMLCanvasElement);
-
+let buscarImgs = true;
 crearMenuCategorias(cargarCategoria);
 
 function cargarCategoria(nombre: string) {
   previsualizador.limpiar();
-  cargarCodigo(`I_${nombre}_40_0001_O.ngc`, 40, true);
+  cargarCodigo(`I_${nombre}_40_O.ngc`, 40, true);
+  if (buscarImgs) buscarImagen(nombre);
 }
 
 async function cargarCodigo(nombre: string, proporcion: number = 40, mostrarTitulo: boolean = false) {
@@ -22,7 +24,7 @@ async function cargarCodigo(nombre: string, proporcion: number = 40, mostrarTitu
 
     if (mostrarTitulo) {
       const partes = nombre.split('_');
-      const nombreTitulo = `T_${partes[1]}_${partes[2]}_${partes[3]}.ngc`;
+      const nombreTitulo = `T_${partes[1]}_${partes[2]}.ngc`;
       const restitulo = await fetch(`/gcodes/${nombreTitulo}`);
 
       if (restitulo.ok) {
