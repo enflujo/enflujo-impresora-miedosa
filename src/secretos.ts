b6654e894c5812dafdb8edaeb2af0f5aec2aa5df
Juan Camilo González <info@juancgonzalez.com>
2025-05-16 11:41:36 -0500

    🚧 no me acuerdo pero parece que estaba combinando texto e imagen

---
+++ src/secretos.ts
@@ -0,0 +1,4 @@
+export const pexelsLlave = 'alD74itVJtRDwReaUBKbyWLCd5YHNl0NlysOnX005cMXiMiR1aVCdYOD';
+export const unsplashLlaveAcceso = 'I3P_ERi2GEzCdbLbK0OzqKKDUgDi6XstfAn-f7O7-hg';
+export const unsplashLlaveSecreta = 'Mz5s-X6XXrFtbdhwzpUow-hMTti7t4C7Ms2Gi96tW5g';
+export const unsplashIDAplicacion = '708004';
